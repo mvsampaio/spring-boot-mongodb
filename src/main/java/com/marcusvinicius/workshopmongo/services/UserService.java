6509000dbdb48091f4d381277c73b785ca0ecc3d
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-05-14 15:54:02 -0300

    getting user by id

--- src/main/java/com/marcusvinicius/workshopmongo/services/UserService.java
+++ src/main/java/com/marcusvinicius/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marcusvinicius.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcusvinicius.workshopmongo.domain.User;
 import com.marcusvinicius.workshopmongo.repository.UserRepository;
+import com.marcusvinicius.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
---
+++ src/main/java/com/marcusvinicius/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.marcusvinicius.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
