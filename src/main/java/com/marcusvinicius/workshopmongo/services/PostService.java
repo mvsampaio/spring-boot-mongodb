ab9e46f75b179dd20150cd19a2353327b01ae0cc
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-05-17 12:30:20 -0300

    Query with multiple criteria

--- src/main/java/com/marcusvinicius/workshopmongo/services/PostService.java
+++ src/main/java/com/marcusvinicius/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.marcusvinicius.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,11 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		return repository.searchTitle(text);
 		//return repository.findByTitleContainingIgnoreCase(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {	
+		//Tratamento para considerar o final do dia seguinte		
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);		
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
